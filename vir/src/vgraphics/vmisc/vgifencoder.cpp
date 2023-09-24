virmodoetiae/shaderthing

commit 3d042d911f903b5cd55644228d9d99ebb97ba57f

vir - added flags to prevent OpenGLKMeansQuantizer from initializing and running if the graphics card in use does not support OpenGL compute shaders (i.e., if OpenGL version is below 4.3), as well as an accessor to a descriptive error message for such scenario; modified GifEncoder (which relies on a KMeansQuantizer) to take aforementioned modifications into account | shaderthing - modified QuantizationTool GUI menu entry to become disabled and display the aforementioned errore message if the graphics card in use does not support OpenGL >= 4.3; modifications to the ExportTool (when GIFs requested as output) are still pending

vir/src/vgraphics/vmisc/vgifencoder.cpp
@@ -157,10 +157,12 @@ GifEncoder::GifEncoder():
     height_(0),
     paletteSize_(0)
 {
+    quantizer_ = vir::KMeansQuantizer::create();
 }
 
 GifEncoder::~GifEncoder()
 {
+    delete quantizer_;
 }
 
 bool GifEncoder::openFile
@@ -196,7 +198,7 @@ bool GifEncoder::openFile
 
     fputs("GIF89a", file_);
 
-    // screen descriptor
+    // Screen descriptor
     fputc(width & 0xff, file_);
     fputc((width >> 8) & 0xff, file_);
     fputc(height & 0xff, file_);
@@ -225,9 +227,6 @@ bool GifEncoder::openFile
     fputc(0, file_); // Loop infinitely (byte 1)
     fputc(0, file_); // Block terminator
 
-    // If we survived til here, initialize the quantizer
-    quantizer_ = vir::KMeansQuantizer::create();
-
     return true;
 }
 
@@ -241,7 +240,7 @@ void GifEncoder::encodeFrame
     bool updatePalette
 )
 {
-    if (quantizer_ == nullptr)
+    if (file_ == nullptr)
         return;
     quantizer_->quantize
     (
@@ -275,6 +274,8 @@ void GifEncoder::encodeFrame
     bool updatePalette
 )
 {
+    if (file_ == nullptr)
+        return;
     quantizer_->quantize
     (
         frame, 
@@ -291,10 +292,6 @@ void GifEncoder::encodeFrame
         false //
     );
     quantizer_->getIndexedTexture(indexedTexture_, firstFrame_);
-    //for (int i=0; i<32; i++)
-    //{
-    //    std::cout << (int)indexedTexture_[i] << " ";
-    //}
     quantizer_->getPalette(palette_, firstFrame_);
     if (firstFrame_)
         firstFrame_ = false;

shaderthing/include/tools/quantizationtool.h
@@ -57,8 +57,10 @@ public:
     void toggleIsGuiInMenu(){isGuiInMenu_ = !isGuiInMenu_;}
     void renderGui();
 
-    bool* isGuiOpenPtr(){return &isGuiOpen_;}
-    bool isGuiInMenu(){return isGuiInMenu_;}
+    bool canRunOnDeviceInUse();
+    const std::string& errorMessage();
+    bool* isGuiOpenPtr() {return &isGuiOpen_;}
+    bool isGuiInMenu() {return isGuiInMenu_;}
     Layer*& targetLayer() {return targetLayer_;}
 };
 

shaderthing/src/layers/layer.cpp
@@ -6,7 +6,6 @@
 #include "tools/quantizationtool.h"
 #include "tools/exporttool.h"
 #include "misc/misc.h"
-
 #include "vir/include/vir.h"
 
 #include <random>

shaderthing/src/shaderthingapp_gui.cpp
@@ -49,6 +49,35 @@ if (cmpt->isGuiInMenu())                                                    \
 }                                                                           \
 else if(ImGui::MenuItem(cmptName, NULL, cmpt->isGuiOpenPtr())){}
 
+#define OZ_MENU_ENTRY_DISABLEABLE(cmpt, cmptName, disable, disableText)     \
+if (disable) ImGui::BeginDisabled();                                        \
+if(ImGui::SmallButton(cmpt->isGuiInMenu() ? "O" : "Z" ))                    \
+    cmpt->toggleIsGuiInMenu();                                              \
+ImGui::SameLine();                                                          \
+if (cmpt->isGuiInMenu())                                                    \
+{                                                                           \
+    if (ImGui::BeginMenu(cmptName))                                         \
+    {                                                                       \
+        if (disable)                                                        \
+        {                                                                   \
+            float textWidth(40.0f*fontSize);                                \
+            ImGui::PushTextWrapPos(40.0f*fontSize);                         \
+            ImGui::Text(disableText);                                       \
+            ImGui::PopTextWrapPos();                                        \
+        }                                                                   \
+        else                                                                \
+        {                                                                   \
+            *(cmpt->isGuiOpenPtr()) = true;                                 \
+            cmpt->renderGui();                                              \
+        }                                                                   \
+        ImGui::EndMenu();                                                   \
+    }                                                                       \
+    else                                                                    \
+        *(cmpt->isGuiOpenPtr()) = false;                                    \
+}                                                                           \
+else if(ImGui::MenuItem(cmptName, NULL, cmpt->isGuiOpenPtr())){}            \
+if (disable) ImGui::EndDisabled(); 
+
 void ShaderThingApp::updateGui()
 {
     // Disable reading/writing from/to imgui.ini
@@ -306,7 +335,13 @@ name##SetBuilt0 = name##SetBuilt;
         }
         if (ImGui::BeginMenu("Effects"))
         {
-            OZ_MENU_ENTRY(quantizationTool_, "Quantizer")
+            OZ_MENU_ENTRY_DISABLEABLE
+            (
+                quantizationTool_, 
+                "Quantizer", 
+                !quantizationTool_->canRunOnDeviceInUse(),
+                quantizationTool_->errorMessage().c_str()
+            )
             ImGui::EndMenu();
         }
         if (ImGui::BeginMenu("Find"))

shaderthing/src/tools/quantizationtool.cpp
@@ -54,6 +54,26 @@ QuantizationTool::~QuantizationTool()
 
 //----------------------------------------------------------------------------//
 
+bool QuantizationTool::canRunOnDeviceInUse() 
+{
+    if (quantizer_ == nullptr)  // Ehhh don't hate on me, If we could only have
+                                // virtual static functions in C++ this would
+                                // not have to look like this
+        quantizer_ = vir::KMeansQuantizer::create();
+    return quantizer_->canRunOnDeviceInUse();
+}
+
+const std::string& QuantizationTool::errorMessage()
+{
+    if (quantizer_ == nullptr)  // Ehhh don't hate on me, If we could only have
+                                // virtual static functions in C++ this would
+                                // not have to look like this
+        quantizer_ = vir::KMeansQuantizer::create();
+    return quantizer_->errorMessage();
+}
+
+//----------------------------------------------------------------------------//
+
 void QuantizationTool::reset()
 {
     isGuiOpen_ = false;

vir/include/vgraphics/vmisc/vgifencoder.h
@@ -47,6 +47,9 @@ public:
     GifEncoder();
     ~GifEncoder();
 
+    bool canRunOnDeviceInUse() const {return quantizer_->canRunOnDeviceInUse();}
+    const std::string& errorMessage() const {return quantizer_->errorMessage();}
+
     bool openFile
     (
         const std::string& filepath, 

vir/include/vgraphics/vmisc/vkmeansquantizer.h
@@ -13,6 +13,15 @@ class KMeansQuantizer
 {
 protected:
 
+    // Depending on the KMeansQuantizer implementation, it might not be able to
+    // run under certain systems. For example, the current implementation of
+    // the OpenGLKMeansQuantizer can only run if the min available OpenGL 
+    // version in use by the host system is OpenGL 4.3, because the current
+    // OpenGL implentation of the quantizer runs entirely off compute shaders,
+    // which are unavailable prior to OGL 4.3
+    bool canRunOnDeviceInUse_;
+    std::string errorMessage_;
+
     // Size of the last quantized image
     uint32_t width_;
     uint32_t height_;
@@ -22,7 +31,11 @@ protected:
     
     // Protected constructor as any instances of KMeansQuantizer are meant to be
     // created via the static create function
-    KMeansQuantizer():width_(0),height_(0),paletteSize_(0){};
+    KMeansQuantizer():
+        canRunOnDeviceInUse_(true),
+        width_(0),
+        height_(0),
+        paletteSize_(0){};
 
 public:
 
@@ -33,6 +46,8 @@ public:
     virtual ~KMeansQuantizer(){};
 
     // Accessors
+    bool canRunOnDeviceInUse() const {return canRunOnDeviceInUse_;}
+    const std::string& errorMessage() const {return errorMessage_;}
     uint32_t width(){return width_;}
     uint32_t height(){return height_;}
     uint32_t paletteSize(){return paletteSize_;}

vir/include/vgraphics/vmisc/vopengl/vopenglkmeansquantizer.h
@@ -15,8 +15,6 @@ class OpenGLKMeansQuantizer : public KMeansQuantizer
 {
 protected:
 
-    bool validOpenGLVersion_;
-
     class ComputeShaderStage
     {
     protected:

commit c38cd2a6e2bccf46b6e3a500f36cf671095a796c

vir - fixed GifEncoder bug introduced in commit 3d042d911

vir/src/vgraphics/vmisc/vgifencoder.cpp
@@ -306,7 +306,6 @@ bool GifEncoder::closeFile()
     fclose(file_);
     file_ = nullptr;
     firstFrame_ = false;
-    delete quantizer_;
     delete[] indexedTexture_;
     delete[] palette_;
     return true;