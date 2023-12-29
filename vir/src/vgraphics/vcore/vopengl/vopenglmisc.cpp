2f35a8ace80a34d2bed64a9db275639ef1fb7ee0
virmodoetiae <sradman@protonmail.com>
2023-12-29 12:24:50 +0100

    vir - minor format and timeout change to vir::OpenGLWaitSync()

--- vir/src/vgraphics/vcore/vopengl/vopenglmisc.cpp
+++ vir/src/vgraphics/vcore/vopengl/vopenglmisc.cpp
@@ -9,7 +9,12 @@ void OpenGLWaitSync()
     auto dataSync = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
     while (dataSync)
     {
-        GLenum wait = glClientWaitSync(dataSync, GL_SYNC_FLUSH_COMMANDS_BIT, 10000);
+        GLenum wait = glClientWaitSync
+        (
+            dataSync, 
+            GL_SYNC_FLUSH_COMMANDS_BIT, 
+            100000 // ns == 100 us timeout
+        );
         if (wait == GL_ALREADY_SIGNALED || wait == GL_CONDITION_SATISFIED)
         {
             glDeleteSync(dataSync);
