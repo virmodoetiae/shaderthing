f55b617039d3d9365f2ba7280a3b3b30f9bc81e1
virmodoetiae <sradman@protonmail.com>
2023-12-29 19:55:11 +0100

    vir - minor edits to OpenGLBloomer, added Karis averaging on first downsampling (but currently disabled), developed and tested new downsampling algorithm which caches texture fetches to reduce overall number of required bilinear fetches and interpolations. However, not used as no significant performance gain could be observed

--- vir/include/vgraphics/vpostprocess/vopengl/vopenglbloomer.h
+++ vir/include/vgraphics/vpostprocess/vopengl/vopenglbloomer.h
@@ -13,7 +13,21 @@ class OpenGLBloomer : public Bloomer
 {
 protected:
 
+    // True if all compute shader stages have been compiled on first class
+    // instantiation
     static bool computeShaderStagesCompiled_;
+
+    // A downsampler which caches texture data for each work group before
+    // downsampling, so to avoid performing a textureLod 13 times for every
+    // single texel. However, it turned out it runs just as well as the 
+    // simpler downsampler_, meaning that the 13 textureLod fetches are
+    // marginal in computational expensiveness in the grand scheme of things.
+    // I am keeping it for reference for future such compute shaders, but
+    // otherwise not used because of its poor legibility/maintainability
+    // compared to the simpler downsampler_;
+    static OpenGLComputeShader cachingDownsampler_;
+
+    // Actually used compute shaders
     static OpenGLComputeShader brightnessMask_;
     static OpenGLComputeShader downsampler_;
     static OpenGLComputeShader upsampler_;
@@ -28,13 +42,13 @@ protected:
 
 public:
 
-    //
+    // Constructor
     OpenGLBloomer();
     
-    //
+    // Destructor
     virtual ~OpenGLBloomer();
 
-    //
+    // Bloom
     void bloom
     (
         const Framebuffer* input,
--- vir/src/vgraphics/vpostprocess/vopengl/vopenglbloomer.cpp
+++ vir/src/vgraphics/vpostprocess/vopengl/vopenglbloomer.cpp
@@ -12,6 +12,90 @@ typedef Bloomer::Settings::ToneMap ToneMap;
 
 bool OpenGLBloomer::computeShaderStagesCompiled_ = false;
 
+//----------------------------------------------------------------------------//
+// Unused compute shaders
+
+OpenGLComputeShader
+    OpenGLBloomer::cachingDownsampler_
+(
+R"(#version 430 core
+#define GROUP_SIZE 8
+#define FILTER_RADIUS 2 // 13-tap downsampling algorithm has a fixed radius of 2
+                                     uniform int       mip;
+                                     uniform ivec2     txsz; // tx size
+                                     uniform ivec2     imsz; // im size
+layout(binding=0)                    uniform sampler2D tx;
+layout(rgba32f, binding=0) writeonly uniform image2D   im;
+layout(local_size_x=GROUP_SIZE, local_size_y=GROUP_SIZE, local_size_z = 1) in;
+shared vec4  data[(GROUP_SIZE+2*FILTER_RADIUS)*(GROUP_SIZE+2*FILTER_RADIUS)];
+       ivec2 gtexel    = ivec2(gl_GlobalInvocationID.xy);
+       ivec2 ltexel    = ivec2(gl_LocalInvocationID.xy);
+       vec2  uv        = (gtexel+.5f)/imsz;
+       vec2  texelSize = 1.f / txsz;
+       ivec2 tileSize  = ivec2(min(8, imsz.x), min(8, imsz.y));
+int dataIndex(int i, int j)
+{
+    return (2*FILTER_RADIUS+tileSize.x)*(2+ltexel.y+j)+(2+ltexel.x+i);
+}
+void cacheData()
+{
+    ivec2 edge;
+    edge.x = ltexel.x == 0 ? -1 : (ltexel.x == tileSize.x-1 ? 1 : 0);
+    edge.y = ltexel.y == 0 ? -1 : (ltexel.y == tileSize.y-1 ? 1 : 0);
+    ivec2 ij;
+    for (int i=0; i<=FILTER_RADIUS*abs(edge.x); i++)
+    {
+        ij.x = i*edge.x;
+        for (int j=0; j<=FILTER_RADIUS*abs(edge.y); j++)
+        {
+            ij.y = j*edge.y;
+            data[dataIndex(ij.x, ij.y)] = 
+                textureLod(tx, vec2(uv + vec2(ij)*texelSize), mip);
+        }
+    }
+}
+vec4 downsample()
+{
+    // Take 13 samples around current texel 'E' from cached data:
+    // a - b - c
+    // - j - k -
+    // d - E - f
+    // - l - m -
+    // g - h - i
+    vec4 a = data[dataIndex(-2, 2)];
+    vec4 b = data[dataIndex( 0, 2)];
+    vec4 c = data[dataIndex( 2, 2)];
+    vec4 d = data[dataIndex(-2, 0)];
+    vec4 E = data[dataIndex( 0, 0)];
+    vec4 f = data[dataIndex( 2, 0)];
+    vec4 g = data[dataIndex(-2,-2)];
+    vec4 h = data[dataIndex( 0,-2)];
+    vec4 i = data[dataIndex( 2,-2)];
+    vec4 j = data[dataIndex(-1, 1)];
+    vec4 k = data[dataIndex( 1, 1)];
+    vec4 l = data[dataIndex(-1,-1)];
+    vec4 m = data[dataIndex( 1,-1)];
+    vec4 color = E*0.125;
+    color += (a+c+g+i)*0.03125;
+    color += (b+d+f+h)*0.0625;
+    color += (j+k+l+m)*0.125;
+    return color;
+}
+void main()
+{
+    if (gtexel.x < imsz.x && gtexel.y < imsz.y)
+    {
+        cacheData();
+        memoryBarrierShared();
+        barrier();
+        imageStore(im, gtexel, downsample());
+    }
+})"
+);
+
+//----------------------------------------------------------------------------//
+// Used compute shaders
+
 OpenGLComputeShader
     OpenGLBloomer::brightnessMask_
     (
@@ -48,17 +132,24 @@ OpenGLComputeShader
     OpenGLBloomer::downsampler_
     (
 R"(#version 430 core
-uniform int mip;
+uniform int   mip;
 uniform ivec2 txsz; // tx size
 uniform ivec2 imsz; // im size
+// uniform bool  kr; // Karis average on/off
 layout(binding=0)                    uniform sampler2D tx;
 layout(rgba32f, binding=0) writeonly uniform image2D   im;
-layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
-vec4 downsample(vec2 uv)
+layout(local_size_x=8, local_size_y=8, local_size_z = 1) in;
+ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
+float karisFactor(vec3 col)
 {
-    vec2 texelSize = 1.0 / txsz; //textureSize(tx, mip);
+    return 1.f/(1.f + dot(col, vec3(0.2126f, 0.7152f, 0.0722f)));
+}
+vec4 downsample()
+{
+    vec2 texelSize = 1.f / txsz;
     float x = texelSize.x;
     float y = texelSize.y;
+    vec2 uv = (texel+.5f)/imsz;
     // Take 13 samples around current texel 'E':
     // a - b - c
     // - j - k -
@@ -78,21 +169,41 @@ vec4 downsample(vec2 uv)
     vec4 k = textureLod(tx, vec2(uv.x   +x, uv.y   +y), mip);
     vec4 l = textureLod(tx, vec2(uv.x   -x, uv.y   -y), mip);
     vec4 m = textureLod(tx, vec2(uv.x   +x, uv.y   -y), mip);
-    // Weigh contributions and return
-    vec4 color = E*0.125;
-    color += (a+c+g+i)*0.03125;
-    color += (b+d+f+h)*0.0625;
-    color += (j+k+l+m)*0.125;
+    vec4 color = 0.125f*E;
+    color += 0.03125f*(a+c+g+i);
+    color += 0.0625f*(b+d+f+h);
+    color += 0.125f*(j+k+l+m);
     return color;
+    /*
+        vec4 color = vec4(0);
+        if (!kr) // No Karis average
+        {
+            color += 0.125f  * E;
+            color += 0.03125f*(a+c+g+i);
+            color += 0.0625f *(b+d+f+h);
+            color += 0.125f  *(j+k+l+m);
+        }
+        else // Karis average (sort-of)
+        {
+            vec4 tmp = (a+b+d+E)/4;
+            color += 0.125f*karisFactor(tmp.rgb)*tmp;
+            tmp =      (b+c+E+f)/4;
+            color += 0.125f*karisFactor(tmp.rgb)*tmp;
+            tmp =      (d+E+g+h)/4;
+            color += 0.125f*karisFactor(tmp.rgb)*tmp;
+            tmp =      (E+f+h+i)/4;
+            color += 0.125f*karisFactor(tmp.rgb)*tmp;
+            tmp =      (j+k+l+m)/4;
+            color += 0.5f*karisFactor(tmp.rgb)*tmp;
+        }
+        return color;
+    */
 }
 void main()
 {
-    ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
     if (texel.x < imsz.x && texel.y < imsz.y)
     {
-        vec2 uv = (vec2(texel)+.5f)/imsz;
-        vec4 col = downsample(uv);
-        imageStore(im, texel, col);
+        imageStore(im, texel, downsample());
     }
 })"
     );
@@ -101,46 +212,40 @@ OpenGLComputeShader
     OpenGLBloomer::upsampler_
     (
 R"(#version 430 core
-uniform int mip;
+uniform int   mip;
 uniform ivec2 txsz; // tx size
 uniform ivec2 imsz; // im size
 uniform float ii;
 uniform float cd;
-uniform int tm;
+uniform int   tm;
 uniform float tma;
 layout(binding=0)          uniform sampler2D tx;
 layout(rgba32f, binding=0) uniform image2D   im;
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
-vec4 upsample(vec2 tc)
+ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
+vec4 upsample()
 {
-    // The filter kernel is applied with a radius, specified in texture
-    // coordinates, so that the radius will vary across mip resolutions.
-    vec2 texelSize = 1.0 / txsz; //textureSize(tx, mip);
+    vec2 texelSize = 1.5 / txsz;
     float x = texelSize.x;
     float y = texelSize.y;
-
-    // Take 9 samples around current texel 'e':
+    vec2 uv = (texel+.5f)/imsz;
+    // Take 9 samples around current texel 'E':
     // a - b - c
-    // d - e - f
+    // d - E - f
     // g - h - i
-    vec4 a = textureLod(tx, vec2(tc.x - x, tc.y + y), mip);
-    vec4 b = textureLod(tx, vec2(tc.x,     tc.y + y), mip);
-    vec4 c = textureLod(tx, vec2(tc.x + x, tc.y + y), mip);
-    vec4 d = textureLod(tx, vec2(tc.x - x, tc.y), mip);
-    vec4 e = textureLod(tx, vec2(tc.x,     tc.y), mip);
-    vec4 f = textureLod(tx, vec2(tc.x + x, tc.y), mip);
-    vec4 g = textureLod(tx, vec2(tc.x - x, tc.y - y), mip);
-    vec4 h = textureLod(tx, vec2(tc.x,     tc.y - y), mip);
-    vec4 i = textureLod(tx, vec2(tc.x + x, tc.y - y), mip);
-
-    // Apply weighted distribution, by using a 3x3 tent filter:
-    //  1   | 1 2 1 |
-    // -- * | 2 4 2 |
-    // 16   | 1 2 1 |
-    vec4 color = e*4.0;
-    color += (b+d+f+h)*2.0;
-    color += (a+c+g+i);
-    color *= 1.0 / 16.0;
+    vec4 a = textureLod(tx, vec2(uv.x - x, uv.y + y), mip);
+    vec4 b = textureLod(tx, vec2(uv.x,     uv.y + y), mip);
+    vec4 c = textureLod(tx, vec2(uv.x + x, uv.y + y), mip);
+    vec4 d = textureLod(tx, vec2(uv.x - x, uv.y), mip);
+    vec4 E = textureLod(tx, vec2(uv.x,     uv.y), mip);
+    vec4 f = textureLod(tx, vec2(uv.x + x, uv.y), mip);
+    vec4 g = textureLod(tx, vec2(uv.x - x, uv.y - y), mip);
+    vec4 h = textureLod(tx, vec2(uv.x,     uv.y - y), mip);
+    vec4 i = textureLod(tx, vec2(uv.x + x, uv.y - y), mip);
+    vec4 color = 4* E;
+    color     += 2*(b+d+f+h);
+    color     +=   (a+c+g+i);
+    color     *= 1.f / 16;
     return color;
 }
 vec3 tonemapReinhard(vec3 c, float ex)
@@ -170,11 +275,9 @@ vec3 tonemapACES(vec3 v)
 }
 void main()
 {
-    ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
     if (texel.x < imsz.x && texel.y < imsz.y)
     {
-        vec2 uv = (vec2(texel)+.5f)/imsz;
-        vec4 col = upsample(uv);
+        vec4 col = upsample();
         col.rgb += imageLoad(im, texel).rgb;
         if (mip == 1) // i.e., final upsamling pass
         {
@@ -344,7 +447,8 @@ void OpenGLBloomer::bloom
 
     // Downsampling ----------------------------------------------------------//
     int minSideResolution = 2; // Could be passed via settings
-    int mipLevel=1;
+    int mipLevel = 1;
+    bool firstLevel = true;
     while (true)
     {
         glBindImageTexture
@@ -378,6 +482,7 @@ void OpenGLBloomer::bloom
         downsampler_.setUniformInt("mip", mipLevel-1);
         downsampler_.setUniformInt2("txsz", mipSize[mipLevel-1], false);
         downsampler_.setUniformInt2("imsz", size, false);
+        //downsampler_.setUniformInt("kr", int(firstLevel), false);
         downsampler_.run
         (
             N_WORK_GROUPS_X(width),
@@ -393,6 +498,8 @@ void OpenGLBloomer::bloom
         )
             break;
         ++mipLevel;
+        if (firstLevel)
+            firstLevel = false;
     }
     int lastMipLevel = mipLevel;
     if (sizeChanged)
